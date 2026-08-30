6b5e85f782388ea6a8c75ebd02371d36a5ae44d3
dennisbrsv <github@noemt.dev>
2026-08-30 13:45:15 +0200

    oopsie

--- server/bot/util/constants.py
+++ server/bot/util/constants.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 GUILD_IDS = 1538276207550931206
 ADMIN_ROLE_ID = 1543546298370621580
 
+def get_guild_ids(): return GUILD_IDS
+
 def is_authorized():
     async def predicate(ctx: discord.ApplicationContext):
         if ctx.author.id in ctx.bot.owner_ids:
